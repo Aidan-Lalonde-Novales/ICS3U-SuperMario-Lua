49e154d3fc36ae357f035825fd030d89f3a7cb73
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-06-21 03:42:56 +0000

    femmm

---
+++ Mario/Mario5/Player.lua
@@ -0,0 +1,64 @@
+-- PLayer Class for Mario
+
+Player = Class{}
+
+function Player:init(map)
+    
+    self.x = 0
+    self.y = 0
+    self.width = 16
+    self.height = 20
+
+    self.xOffset = 8
+    self.yOffset = 16
+
+    self.map = map
+    self.texture = love.graphics.newImage('graphics/blue_alien.png')
+
+    -- animation stuff
+    self.frames = {}
+    self.currentFrame = nil
+    self.state = 'idle'
+    self.direction = 'left'
+
+    self.dx = 0
+    self.dy = 0
+
+    self.y = map.tileHeight * ((map.mapHeight - 2) / 2) - self.height
+    self.x = map.tileWidth * 10
+
+    self.frames = {
+        love.graphics.newQuad(0, 0, 16, 20, self.texture:getDimensions())
+    }
+
+    self.currentFrame = self.frames[1]
+
+    self.behaviors = {
+        ['idle'] = function(dt)
+            
+            if love.keyboard.wasPressed('left') then
+                direction = 'left'
+            end
+            if love.keyboard.wasPressed('right') then
+                direction = 'right'
+            end
+        end
+    }
+end
+
+function Player:update(dt)
+    self.behaviors[self.state](dt)
+end
+
+function Player:render()
+    local scaleX
+
+    if self.direction == 'right' then
+        scaleX = 1
+    else
+        scaleX = -1
+    end
+
+    love.graphics.draw(self.texture, self.currentFrame, self.x + self.xOffset,
+        self.y + self.yOffset, 0, scaleX, 1, self.xOffset, self.yOffset)
+end

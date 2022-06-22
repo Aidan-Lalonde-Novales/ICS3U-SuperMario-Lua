db556bdb0b56ec2fbbd881211b7927a95f0a6c51
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-06-22 03:47:16 +0000

    maro

--- Mario/Mario9/main.lua
+++ Mario/Mario9/main.lua
@@ -1,10 +1,4 @@
---[[
-    Super Mario Bros. Demo
-    Author: Colton Ogden
-    Original Credit: Nintendo
-
-    Demonstrates rendering a screen of tiles.
-]]
+-- Main Code for Mario 8 / Learning Guide 3.6
 
 Class = require 'class'
 push = require 'push'
@@ -13,30 +7,23 @@ require 'Animation'
 require 'Map'
 require 'Player'
 
--- close resolution to NES but 16:9
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
--- actual window resolution
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
--- seed RNG
+-- RNG
 math.randomseed(os.time())
 
--- an object to contain our map data
 map = Map()
 
--- performs initialization of all objects and data needed by program
 function love.load()
     
-    -- makes upscaling look pixel-y instead of blurry
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    -- sets up a different, better-looking retro font as our default
     love.graphics.setFont(love.graphics.newFont('fonts/font.ttf', 8))
 
-    -- sets up virtual screen resolution for an authentic retro feel
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = true
@@ -46,7 +33,6 @@ function love.load()
     love.keyboard.keysReleased = {}
 end
 
--- called whenever window is resized
 function love.resize(w, h)
     push:resize(w, h)
 end
@@ -69,41 +55,32 @@ function love.keyboard.wasReleased(key)
     end
 end
 
--- called whenever a key is pressed
+
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
-
     love.keyboard.keysPressed[key] = true
 end
 
--- called whenever a key is released
 function love.keyreleased(key)
     love.keyboard.keysReleased[key] = true
 end
 
--- called every frame, with dt passed in as delta in time since last frame
 function love.update(dt)
     map:update(dt)
 
-    -- reset all keys pressed and released this frame
     love.keyboard.keysPressed = {}
     love.keyboard.keysReleased = {}
 end
 
--- called each frame, used to render to the screen
 function love.draw()
-    -- begin virtual resolution drawing
     push:apply('start')
 
-    -- clear screen using Mario background blue
-    love.graphics.clear(108, 140, 255, 255)
+    love.graphics.clear(108 / 255, 140 / 255, 255 / 255, 255 / 255)
 
-    -- renders our map object onto the screen
     love.graphics.translate(math.floor(-map.camX + 0.5), math.floor(-map.camY + 0.5))
     map:render()
 
-    -- end virtual resolution
     push:apply('end')
 end
